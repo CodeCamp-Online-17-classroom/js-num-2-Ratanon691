b08e94118c5970e214095d914b51ad4a6e777b5e
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-28 18:10:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-// write code here
+let num = 255
+let binary = num.toString(2)
+let octal = num.toString(8)
+let Hexadecimal = num.toString(16)
+console.log(`255 in binary is ${binary}, in Octal is ${octal}, in Hexadecimal is ${Hexadecimal}`)
+  
